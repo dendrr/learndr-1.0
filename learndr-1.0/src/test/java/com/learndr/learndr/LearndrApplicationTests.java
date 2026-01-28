2e8f7e02a429e0b88a456ede2c6768b237558c2e
Dudka Denys <52039077+dendrr@users.noreply.github.com>
2026-01-28 16:41:23 +0100

    Add files via upload

---
+++ learndr-1.0/src/test/java/com/learndr/learndr/LearndrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learndr.learndr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearndrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
